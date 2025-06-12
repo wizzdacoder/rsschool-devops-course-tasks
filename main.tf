9a83b4b64fb85334a0805a0243c942e51d8f5764
Alan Demian Beltramo <109772077+wizzdacoder@users.noreply.github.com>
2025-06-12 14:54:15 -0300

    add terraform main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "task1-s3-bucket" {
+  bucket = "my-task1-s3-bucket-777"
+  acl = "private"
+
+  tags = {
+    Name = "MyTaskBucket"
+  }
+}
